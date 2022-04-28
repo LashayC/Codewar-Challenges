80a418bb6cfdc4258bf0fb4576f8e30bbb110ac4
LashayC <cokleylashay@gmail.com>
2022-04-27 21:15:17 -0400

    level 8 kyu push

---
+++ level_8_kyu/Calculate average.js
@@ -0,0 +1,9 @@
+function find_average(array) {
+    if(array.length === 0){
+      return 0
+    }else{
+      let sum = 0
+      array.forEach(el => sum += el)
+      return sum / (array.length)
+      }
+  }
